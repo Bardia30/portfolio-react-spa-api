82c6a07ea381a856e9c105f8f096958339828003
Bardia Dehbasti <bardiadehbasti@gmail.com>
2023-09-24 12:19:18 -0400

    restart the project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 
 app = express();
 
@@ -15,10 +16,34 @@ app.use(express.json());
 
 
 
-
+//endpoint to send the all the projects
 app.get('/', (req, res)=> {
-    res.send("Welcome to Bardia Dehbasti's Portfolio backend server");
+    fs.readFile('./data/projects.json', 'utf8', (err, data)=> {
+        if (err) {
+            return res.send(err);
+        }
+        const projectsData = JSON.parse(data);
+        res.status(200).json(projectsData);
+    })
 })
+
+
+
+
+//endpoint to send all the skills 
+app.get('/skills', (req, res)=> {
+    fs.readFile('./data/skills.json', 'utf8', (err, data)=> {
+        if (err) {
+            return res.send(err);
+        }
+        const skillsData = JSON.parse(data);
+        res.status(200).json(skillsData);
+    })
+})
+
+
+
+
 
 const PORT = process.env.PORT || 5051;
 
